8720b9db5965bbfc74a2a71ed0906d942e72f887
HarshitArora07 <162539536+HarshitArora07@users.noreply.github.com>
2024-11-20 23:12:50 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,15 @@
+// src/App.js
+import React from 'react';
+import './App.css';
+import Counter from './components/Counter';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Interactive Counter App</h1>
+      <Counter />
+    </div>
+  );
+};
+
+export default App;
